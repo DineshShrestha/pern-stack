0e3b0d15743ab7e432d15cdf26f2aa434c8b7c61
Dinesh Shrestha <dinu.shrestha777@gmail.com>
2025-06-09 17:05:55 +0200

    Bot Detection and rate limiting

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,38 @@ app.use(express.json());
 app.use(cors())
 app.use(helmet()); // Helmet is a security middleware that helps protect your app by setting vrious HTTP headers
 app.use(morgan("dev")); // log the request
+// apply arcjet rate-limit to all routes
+app.use(async(req, res, next)=>{
+    try {
+        const decision = await aj.protect(req, {
+            requested:1
+        })
+        if(decision.isDenied()){
+            if(decision.reason.isRateLimit()){
+                res.status(429).json({
+                    error: "Rate limit exceeded"
+                })
+            }else if(decision.reason.isBot()){
+                res.status(403).json({
+                    error: "Bot access denied"
+                })
+            }else {
+                res.status(403).json({error: "Forbidden"})
+            }
+            return
+        }
+
+        //check for spoofed bots
+        if(decision.results.some((result)=>result.reason.isBot() && result.reason.isSpoofed())){
+            res.status(403).json({error: "Spoofed bot detected"})
+            return;
+        }
+        next()
+    } catch (error) {
+        console.log("Arcjet error", error);
+        next(error)
+    }
+})
 
 app.use("/api/products", productRoutes);
 async function initDB() {
